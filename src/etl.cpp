5d8951eba14a41596531620cfa0fcfc03d7a5e49
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2022-04-03 12:51:57 -0700

    Added then removed data cleaning

--- src/etl.cpp
+++ src/etl.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
 #include "../headers/etl.h"
 #include <boost/algorithm/string.hpp>
 #include <eigen3/Eigen/Dense>
 
 
+
 std::vector<std::vector<std::string>> ETL::ReadCSV()
 {
+    /*Reads csv data into memory as vector of vector of strings*/
     std::fstream file(filename);
     std::vector<std::vector<std::string>> dataString;
     
@@ -23,6 +26,23 @@ std::vector<std::vector<std::string>> ETL::ReadCSV()
     file.close();
 
     return dataString;
+}
+
+void ETL::CorrectDataFormatting(std::vector<std::vector<std::string>> inputData, std::string outputFile)
+{
+    /*Corrects formatting issue of data/housing.csv and writes
+    data/houseingCleaned.txt*/
+    std::fstream file;
+    file.open(outputFile, std::fstream::out);
+
+    for (int i= 0; i < inputData.size(); i++)
+    {
+        file << inputData[i] << ",";
+        file<< std::endl;
+
+    }
+    file.close();
+    
 }
 
 Eigen::MatrixXd ETL::CSVtoEigen(std::vector<std::vector<std::string>> dataset, int rows, int cols)
